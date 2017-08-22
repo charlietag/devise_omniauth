8e81da9167c26fd1c1e86fcbd20758dbd2e75b6c
Charlie Hsieh <charlieofficial@gmail.com>
2017-08-22 13:32:21 +0800

    only creator can edit or delete

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
-      can :manage, :all
+      #can :manage, :all do |s|   #<--- not working for create
+      can [:update, :destroy], :all , user_id: user.id #<---- official wiki example
+      #---> learned from https://www.youtube.com/watch?v=WNfYQZWNvbk&index=4&list=PL6HtmRWwDtYMuIDt4syZdGXWFPZrAWvm_#t=15.132344
+      #---> this also works
+      #can [:update, :destroy], :all do |s|
+      #  s.user==user
+      #end
+      can [:read, :create], :all
     else
       can :read, :all
     end
