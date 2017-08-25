0c48abad3dab37ed8af940f904038f473ebe1695
Charlie Hsieh <charlieofficial@gmail.com>
2017-08-25 23:26:44 +0800

    add more priv usage using cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,15 +3,30 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    #if user.has_role? :admin
+    #  #can :manage, :all do |s|   #<--- not working for create
+    #  can [:update, :destroy], :all , user_id: user.id #<---- official wiki example
+    #  #---> learned from https://www.youtube.com/watch?v=WNfYQZWNvbk&index=4&list=PL6HtmRWwDtYMuIDt4syZdGXWFPZrAWvm_#t=15.132344
+    #  #---> this also works
+    #  #can [:update, :destroy], :all do |s|
+    #  #  s.user==user
+    #  #end
+    #  can [:read, :create], :all
+    #else
+    #  can :read, :all
+    #end
+
     if user.has_role? :admin
+      can :manage, :all 
       #can :manage, :all do |s|   #<--- not working for create
-      can [:update, :destroy], :all , user_id: user.id #<---- official wiki example
       #---> learned from https://www.youtube.com/watch?v=WNfYQZWNvbk&index=4&list=PL6HtmRWwDtYMuIDt4syZdGXWFPZrAWvm_#t=15.132344
       #---> this also works
       #can [:update, :destroy], :all do |s|
       #  s.user==user
       #end
+    elsif user.has_role? :member
       can [:read, :create], :all
+      can [:update, :destroy], :all , user_id: user.id #<---- official wiki example
     else
       can :read, :all
     end
